d9c0afdbd7205c2c816438a50d559daab071dcdb
Rayssa <rayssarrsilva1@gmail.com>
2025-05-26 15:59:24 -0300

    refactor: Organizar a ordem das URLs no app monitor

--- monitor/urls.py
+++ monitor/urls.py
@@ -2,18 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.homepage, name='homepage'),
+    path('login/', views.fazer_login, name='login'),
+    path('logout/', views.fazer_logout, name='logout'),
     path('urls/', views.lista_urls, name='lista_urls'),
     path('urls/criar/', views.criar_url, name='criar_url'),
     path('urls/<int:id>/excluir/', views.excluir_url, name='excluir_url'),
     path('notificacoes/', views.lista_notificacoes, name='notificacoes'),
-    path('acessos/', views.historico_acessos, name='historico_acessos'),
-    path('registrar/', views.registrar, name='registrar'),
-    path('login/', views.fazer_login, name='login'),
-    path('logout/', views.fazer_logout, name='logout'),
-    path('', views.homepage, name='homepage'),
     path('notificacoes/desativar/<int:url_id>/', views.desativar_notificacoes, name='desativar_notificacoes'),
     path('notificacoes/reativar/<int:url_id>/', views.reativar_notificacoes, name='reativar_notificacoes'),
-
-
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('acessos/', views.historico_acessos, name='historico_acessos'),
+    path('registrar/', views.registrar, name='registrar'),
 ]
